de1cd00e658497d6316989df5f119f85984f0e6c
vascoalramos <vascoalramos@outlook.pt>
2019-10-27 19:30:04 +0000

    Lab03 -> ex02 | ex03

---
+++ lab03/ies-rest-demo/src/test/java/com/vasco/iesrestdemo/IesRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasco.iesrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IesRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
